505b6bcbcf2f431e569e282b920c17861d66abce
yuniquintero <yuni.quintero@gmail.com>
2025-12-04 15:59:04 +0100

    Initial Commit

---
+++ YuniQuintero-BitcoinTracker/YuniQuintero_BitcoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuniQuintero_BitcoinTrackerApp.swift
+//  YuniQuintero-BitcoinTracker
+//
+//  Created by Yuni Quintero on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct YuniQuintero_BitcoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
